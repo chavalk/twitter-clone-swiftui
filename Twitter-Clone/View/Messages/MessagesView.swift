5dbd325e40a4cdf8b98cea91409c5b31657d26d4
chavalk <chavalk@hotmail.com>
2023-03-08 09:33:54 -0600

    Add views for each tab item and set each view with its corresponding tab item.

---
+++ Twitter-Clone/View/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  Twitter-Clone
+//
+//  Created by Jose Garcia on 3/8/23.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
