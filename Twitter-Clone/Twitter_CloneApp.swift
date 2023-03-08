821297ee56f0cb158b315aac7f797588338fd927
chavalk <chavalk@hotmail.com>
2023-03-08 08:35:16 -0600

    First commit.

---
+++ Twitter-Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter-Clone
+//
+//  Created by Jose Garcia on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
