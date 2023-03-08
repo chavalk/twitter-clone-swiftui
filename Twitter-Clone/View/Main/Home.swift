4cb06b6a3f5fc1a10916cfd6615cc77e36805998
chavalk <chavalk@hotmail.com>
2023-03-08 09:00:43 -0600

    Add tab view in homeview.

--- Twitter-Clone/View/Main/Home.swift
+++ Twitter-Clone/View/Main/Home.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack{
+                TabView {
+                    Text("Feed")
+                        .tabItem {
+                            Image("Home")
+                        }
+                }
+            }
+        }
     }
 }
 
