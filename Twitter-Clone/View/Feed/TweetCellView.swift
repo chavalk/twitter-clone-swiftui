b46a2f8975bddb60319415cc6fb75ef1b6b63f23
chavalk <chavalk@hotmail.com>
2023-03-08 11:53:40 -0600

    Add tweet cell view file.

---
+++ Twitter-Clone/View/Feed/TweetCellView.swift
@@ -0,0 +1,44 @@
+//
+//  TweetCellView.swift
+//  Twitter-Clone
+//
+//  Created by Jose Garcia on 3/8/23.
+//
+
+import SwiftUI
+
+struct TweetCellView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 10) {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 55, height: 55)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    (
+                        Text("Cem ")
+                            .fontWeight(.bold)
+                            .foregroundColor(.primary)
+                        +
+                        Text("@cem_salta")
+                            .foregroundColor(.gray)
+                    )
+                    
+                    Text(sampleText)
+                        .frame(maxHeight: 100, alignment: .top)
+                }
+            }
+        }
+    }
+}
+
+struct TweetCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetCellView()
+    }
+}
+
+var sampleText = "Duis pellentesque purus sit amet tortor commodo eleifend. Pellentesque condimentum in sem a dignissim. Ut pharetra faucibus ex vel pretium. Cras dignissim feugiat ante ac maximus. Maecenas non interdum nunc, at ornare dolor. Aliquam sagittis, nulla ut euismod placerat, nisi felis eleifend elit, et varius erat nisi vel nisl. Nam ut tristique ligula, nec laoreet quam. Praesent ut viverra velit, eget fermentum libero."
