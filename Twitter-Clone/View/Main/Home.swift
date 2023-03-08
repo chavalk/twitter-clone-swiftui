2dc6cd40a36aac197bb253e9b0f8556cf91367dc
chavalk <chavalk@hotmail.com>
2023-03-08 08:55:31 -0600

    Add home file to main folder.

---
+++ Twitter-Clone/View/Main/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  Twitter-Clone
+//
+//  Created by Jose Garcia on 3/8/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
