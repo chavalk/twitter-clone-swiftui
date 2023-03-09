454d610e5b3616a633dcf7408146e046a7010d6d
chavalk <chavalk@hotmail.com>
2023-03-08 21:32:26 -0600

    Add vstack to hold text labels.

--- Twitter-Clone/View/Search/SearchCell.swift
+++ Twitter-Clone/View/Search/SearchCell.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct SearchCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 5) {
+            Text("hello")
+                .fontWeight(.heavy)
+            Text("hello")
+        }
     }
 }
 
