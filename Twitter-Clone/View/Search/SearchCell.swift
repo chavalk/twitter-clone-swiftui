b82f8a3e3cc67b558027000c61c1a114b8f0c045
chavalk <chavalk@hotmail.com>
2023-03-08 21:28:13 -0600

    Add search cell file.

---
+++ Twitter-Clone/View/Search/SearchCell.swift
@@ -0,0 +1,20 @@
+//
+//  SearchCell.swift
+//  Twitter-Clone
+//
+//  Created by Jose Garcia on 3/8/23.
+//
+
+import SwiftUI
+
+struct SearchCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchCell_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchCell()
+    }
+}
