e605d2ce4b0b4900c8f099ad9d54c96d1cb065b4
chavalk <chavalk@hotmail.com>
2023-03-08 21:51:29 -0600

    Add hstack with text field.

--- Twitter-Clone/View/Items/SearchBar.swift
+++ Twitter-Clone/View/Items/SearchBar.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct SearchBar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            TextField("Search Twitter", text: <#T##Binding<String>#>)
+        }
     }
 }
 
